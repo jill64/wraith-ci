83a913137837aab66165994fa3a13a25b0e83a26
jill64 <intents.turrets0h@icloud.com>
2023-11-06 18:13:40 +0900

    fix: tests

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -3,7 +3,9 @@ import { expect, test } from '@playwright/test'
 test('smoke', async ({ page }) => {
   await page.goto('/')
 
-  await expect(
-    page.getByRole('heading', { name: 'Svelte App Template' })
-  ).toBeVisible()
+  await expect(page.getByRole('heading', { name: 'Wraith CI' })).toBeVisible()
+
+  await page.goto('/en')
+
+  await expect(page.getByText('WIP')).toBeVisible()
 })
