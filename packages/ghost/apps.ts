638524b0f75700e7f4ff4d849be269bb5d506f58
jill64 <intents.turrets0h@icloud.com>
2023-11-06 22:49:35 +0900

    Add apps module with Ghost type and app functions

---
+++ packages/ghost/apps.ts
@@ -0,0 +1,20 @@
+import { Ghost } from '@/types/Ghost.js'
+import { build } from './app/build.js'
+import { closer } from './app/closer.js'
+import { deploy } from './app/deploy.js'
+import { docs } from './app/docs.js'
+import { format } from './app/format.js'
+import { lint } from './app/lint.js'
+import { merge } from './app/merge.js'
+import { release } from './app/release.js'
+
+export const apps: Record<string, Ghost> = {
+  build,
+  closer,
+  deploy,
+  docs,
+  format,
+  lint,
+  merge,
+  release
+}
