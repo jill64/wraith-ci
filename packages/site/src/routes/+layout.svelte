0afd5fe1a14f7b680670e9cd37952574494da649
jill64 <intents.turrets0h@icloud.com>
2023-11-06 18:18:18 +0900

    Add suffix variable to page title.

--- packages/site/src/routes/+layout.svelte
+++ packages/site/src/routes/+layout.svelte
@@ -9,6 +9,7 @@
   $: title = 'Wraith CI'
   $: description = '👻 CI suite in the dark'
   $: suffix = $theme === 'dark' ? '-dark' : ''
+  $: suffix
 </script>
 
 <Toaster />
@@ -22,9 +23,9 @@
   image="{$page.url.origin}/og-image.png"
 />
 <svelte:head>
-  <link rel="icon" href="/favicon{suffix}.png" />
+  <!-- <link rel="icon" href="/favicon{suffix}.png" />
   <link rel="icon" href="/favicon{suffix}.svg" type="image/svg+xml" />
-  <link rel="apple-touch-icon" href="/apple-touch-icon{suffix}.png" />
+  <link rel="apple-touch-icon" href="/apple-touch-icon{suffix}.png" /> -->
   <title>{title}</title>
   <meta name="description" content={description} />
 </svelte:head>
