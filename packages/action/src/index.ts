56c3220305a9d5a50b13b723b62341262e77d283
jill64 <intents.turrets0h@icloud.com>
2023-11-06 20:37:24 +0900

    Add ghosts action and WraithPayload type, and use attempt and core.getInput in action

--- packages/action/src/index.ts
+++ packages/action/src/index.ts
@@ -1,5 +1,14 @@
+import { action as ghosts } from '@/ghost/action.js'
+import { WraithPayload } from '@/types/WraithPayload.js'
+import { attempt } from '@jill64/attempt'
 import { action } from 'octoflare/action'
+import * as core from 'octoflare/action/core'
 
 action(async () => {
-  return 'success' as const
+  const data = attempt(() => {
+    const str = core.getInput('data')
+    return JSON.parse(str) as WraithPayload
+  })
+  
+  await ghosts(data)
 })
