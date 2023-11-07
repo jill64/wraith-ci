d25fd3bb358d4f828560e0d66aa2adad79af1186
jill64 <intents.turrets0h@icloud.com>
2023-11-07 17:19:46 +0900

    Add ref property to ActionContext and WorkerContext types

--- packages/types/ActionContext.ts
+++ packages/types/ActionContext.ts
@@ -2,6 +2,7 @@ import { workers } from '@/ghost/workers.js'
 import { ActionOctokit } from 'octoflare/action'
 
 export type ActionContext = {
+  ref: string
   repo: string
   owner: string
   octokit: ActionOctokit
