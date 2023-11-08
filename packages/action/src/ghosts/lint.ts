5abe95da31327f2aec63384fc7c48c829050c3a2
jill64 <intents.turrets0h@icloud.com>
2023-11-08 16:17:44 +0900

    Add linting functionality to Ghosts

--- packages/action/src/ghosts/lint.ts
+++ packages/action/src/ghosts/lint.ts
@@ -1,5 +1,100 @@
 import { Ghost } from '@/action/types/Ghost.js'
+import { attempt } from '@jill64/attempt'
+import { writeFile } from 'fs/promises'
+import { array, optional, scanner, string } from 'typescanner'
+import { failedSummary } from '../utils/failedSummary.js'
+import { run } from '../utils/run.js'
+import { syncChanges } from '../utils/syncChanges.js'
 
-export const lint: Ghost = async () => {
-  return 'success'
+export const lint: Ghost = async ({ payload, octokit }) => {
+  const lintResult = await run('npm run lint')
+
+  if (lintResult.exitCode === 0) {
+    return 'success'
+  }
+
+  const isDepcheckError =
+    lintResult.stdout.includes('Unused dependencies') ||
+    lintResult.stdout.includes('Unused devDependencies')
+
+  if (isDepcheckError) {
+    const { stdout: packageJsonStr } = await run('cat package.json')
+
+    const packageJson = attempt(
+      () => JSON.parse(packageJsonStr) as unknown,
+      null
+    )
+
+    const isValidPackageJson = scanner({
+      dependencies: optional(scanner({})),
+      devDependencies: optional(scanner({}))
+    })
+
+    if (!isValidPackageJson(packageJson)) {
+      return {
+        conclusion: 'failure',
+        output: {
+          title: 'Invalid Package.json',
+          summary: JSON.stringify(packageJson, null, 2)
+        }
+      }
+    }
+
+    const depcheckResult = await run('npx depcheck --json')
+    const result = JSON.parse(depcheckResult.stdout)
+
+    const isValidResult = scanner({
+      dependencies: array(string),
+      devDependencies: array(string)
+    })
+
+    if (!isValidResult(result)) {
+      return {
+        conclusion: 'failure',
+        output: {
+          title: 'Invalid Depcheck Result',
+          summary: depcheckResult.stdout
+        }
+      }
+    }
+
+    const omittedDeps = Object.fromEntries(
+      Object.entries(packageJson.dependencies ?? {}).filter(
+        ([key]) => !result.dependencies.includes(key)
+      )
+    )
+
+    const omittedDevDeps = Object.fromEntries(
+      Object.entries(packageJson.devDependencies ?? {}).filter(
+        ([key]) => !result.devDependencies.includes(key)
+      )
+    )
+
+    const omittedPackageJson = {
+      ...packageJson,
+      ...(Object.keys(omittedDeps).length ? { dependencies: omittedDeps } : {}),
+      ...(Object.keys(omittedDevDeps).length
+        ? { devDependencies: omittedDevDeps }
+        : {})
+    }
+
+    await writeFile('package.json', JSON.stringify(omittedPackageJson, null, 2))
+
+    await syncChanges({
+      message: 'fix: omit unused dependencies',
+      branch: 'wraith-ci/ghost-lint',
+      octokit,
+      payload
+    })
+
+    return {
+      conclusion: 'failure',
+      output: {
+        title: 'Omitted Unused Dependencies',
+        summary: 'New package.json has been pushed.'
+      }
+    }
+  }
+
+  return failedSummary('Lint Failed', lintResult)
 }
--- packages/worker/src/ghosts/lint.ts
+++ packages/worker/src/ghosts/lint.ts
@@ -1,3 +1,57 @@
 import { Ghost } from '@/worker/types/Ghost.js'
+import { scanner, string } from 'typescanner'
 
-export const lint: Ghost = async () => {}
+const isValidJson = scanner({
+  scripts: scanner({
+    lint: string
+  })
+})
+
+export const lint: Ghost = async ({
+  installation,
+  ref,
+  payload,
+  createCheckRun
+}) => {
+  if (!('commits' in payload)) {
+    return
+  }
+
+  const check_run_id = await createCheckRun('Ghost Lint')
+
+  if (!check_run_id) {
+    return
+  }
+
+  const file = await installation.getFile('package.json', {
+    ref,
+    parser: (x) => {
+      const json = JSON.parse(x)
+      return isValidJson(json) ? json : null
+    }
+  })
+
+  if (!file) {
+    return {
+      conclusion: 'skipped',
+      output: {
+        title: 'No package.json',
+        summary: 'Not found package.json in repo.'
+      }
+    }
+  }
+
+  if (!file.data?.scripts.lint) {
+    return {
+      conclusion: 'skipped',
+      output: {
+        title: 'No Lint Command',
+        summary: 'Lint command not found in package.json.'
+      }
+    }
+  }
+
+  return {
+    check_run_id
+  }
+}
