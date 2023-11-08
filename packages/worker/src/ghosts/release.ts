d6837690ff3b81598ebfde76fbd12bcd7b0d701e
jill64 <intents.turrets0h@icloud.com>
2023-11-08 17:14:39 +0900

    feat ghost release

--- packages/worker/src/ghosts/release.ts
+++ packages/worker/src/ghosts/release.ts
@@ -1,3 +1,43 @@
 import { Ghost } from '@/worker/types/Ghost.js'
+import { scanner, string } from 'typescanner'
 
-export const release: Ghost = async () => {}
+const isValidJson = scanner({
+  version: string
+})
+
+export const release: Ghost = async ({
+  payload,
+  ref,
+  repository,
+  installation,
+  createCheckRun
+}) => {
+  if (!('commits' in payload)) {
+    return
+  }
+
+  if (ref !== repository.default_branch) {
+    return
+  }
+
+  const response = await installation.getFile('package.json', {
+    parser: (str) => {
+      const json = JSON.parse(str)
+      return isValidJson(json) ? json : null
+    }
+  })
+
+  if (!response?.data?.version) {
+    return
+  }
+
+  const check_run_id = await createCheckRun('Ghost Release')
+
+  if (!check_run_id) {
+    return
+  }
+
+  return {
+    check_run_id
+  }
+}
