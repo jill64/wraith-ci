e159c1d9c0054c0ee05839582335c7f3ce4615d1
jill64 <intents.turrets0h@icloud.com>
2023-11-08 10:55:51 +0900

    Add apps module with Ghost functions.

---
+++ packages/action/src/apps.ts
@@ -0,0 +1,15 @@
+import { GhostName } from '@/shared/types/GhostName.js'
+import { Ghost } from '../types/Ghost.js'
+import { build } from './ghosts/build.js'
+import { deploy } from './ghosts/deploy.js'
+import { format } from './ghosts/format.js'
+import { lint } from './ghosts/lint.js'
+import { release } from './ghosts/release.js'
+
+export const apps = {
+  build,
+  deploy,
+  format,
+  lint,
+  release
+} satisfies Record<Exclude<GhostName, 'docs' | 'merge'>, Ghost>
