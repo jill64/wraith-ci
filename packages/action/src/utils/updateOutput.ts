9475f1dca56f80c44b09d20aa4b4fae10fa498ef
jill64 <intents.turrets0h@icloud.com>
2023-11-10 00:01:25 +0900

    Add updateOutput test and implementation

---
+++ packages/action/src/utils/updateOutput.ts
@@ -0,0 +1,40 @@
+import { getStatusEmoji } from '@/shared/src/getStatusEmoji.js'
+import { schema } from '@/shared/src/schema.js'
+import { GhostName } from '@/shared/types/GhostName.js'
+import { GhostStatusShortHand } from '@/shared/types/GhostStatusShortHand.js'
+import { ChecksOutput } from 'octoflare'
+
+export const updateOutput = ({
+  ghost_name,
+  result,
+  output
+}: {
+  ghost_name: GhostName
+  result: GhostStatusShortHand
+  output: {
+    title: string
+    summary: string
+  }
+}): ChecksOutput => {
+  const alias = schema[ghost_name].alias
+  const ghost_status = typeof result === 'string' ? { status: result } : result
+  const status_emoji = getStatusEmoji(ghost_status)
+
+  const title = output.title.replace(
+    new RegExp(`.* ${alias}`),
+    `${status_emoji} ${alias}`
+  )
+
+  const summary = output.summary.replace(
+    new RegExp(`\\| ${alias} \\| .* \\| .* \\|`),
+    `| ${alias} | ${status_emoji} ${ghost_status.status} | ${
+      ghost_status.detail ?? ''
+    } |`
+  )
+
+  return {
+    ...output,
+    title,
+    summary
+  }
+}
