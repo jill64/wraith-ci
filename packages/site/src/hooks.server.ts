0007bed9b48d0f842c707ec5402b80a51ff30170
jill64 <143883742+jill64@users.noreply.github.com>
2023-11-22 02:34:37 +0000

    Update Sentry DSN in client and server hooks

--- packages/site/src/hooks.server.ts
+++ packages/site/src/hooks.server.ts
@@ -1,11 +1,12 @@
-import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 import { attach as lang_attach } from '$lib/i18n'
 import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
 import { onRender } from '@jill64/svelte-dark-theme'
 import { attach as ogp_attach } from '@jill64/svelte-ogp'
 import { sequence } from '@sveltejs/kit/hooks'
 
-const { onHandle, onError } = serverInit(PUBLIC_SENTRY_DSN)
+const { onHandle, onError } = serverInit(
+  'https://a879bb63f90aea278cbc009cc0e5faca@o4505814639312896.ingest.sentry.io/4506177049067520'
+)
 
 export const handle = onHandle(sequence(lang_attach, onRender(), ogp_attach))
 export const handleError = onError()
