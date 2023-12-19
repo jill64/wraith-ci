50ba38caf1aa5d6b579faa7e9a26b2b4d1d0e995
jill64 <intents.turrets0h@icloud.com>
2023-12-19 18:57:39 +0900

    def Ghost Assign

---
+++ packages/action/src/ghosts/assign.ts
@@ -0,0 +1,49 @@
+import { Ghost } from '@/action/types/Ghost.js'
+
+export const assign: Ghost = async ({ payload, octokit }) => {
+  const {
+    repo,
+    owner,
+    data: { pull_number }
+  } = payload
+
+  if (!pull_number) {
+    return 'skipped'
+  }
+
+  const { data: pull_request } = await octokit.rest.pulls.get({
+    owner,
+    repo,
+    pull_number
+  })
+
+  if (pull_request.user.login === owner) {
+    return {
+      status: 'skipped',
+      reason: 'Pull Request is by Owner'
+    }
+  }
+
+  if (
+    pull_request.requested_reviewers?.some(
+      (user) => 'login' in user && user.login === owner
+    )
+  ) {
+    return {
+      status: 'skipped',
+      reason: 'Owner is already a reviewer'
+    }
+  }
+
+  await octokit.rest.pulls.requestReviewers({
+    owner,
+    repo,
+    pull_number: pull_request.number,
+    reviewers: [
+      ...(pull_request.requested_reviewers ?? []).map((user) => user.login),
+      owner
+    ]
+  })
+
+  return 'success'
+}
