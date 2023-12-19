1cc88d6e93f35b2342a5054e1dda3cc6ac59d8af
jill64 <intents.turrets0h@icloud.com>
2023-12-19 18:41:38 +0900

    ref ghosts

--- packages/action/src/ghosts/build.ts
+++ packages/action/src/ghosts/build.ts
@@ -1,9 +1,33 @@
 import { Ghost } from '@/action/types/Ghost.js'
+import { scanner, string } from 'typescanner'
+import { getPackageJson } from '../utils/getPackageJson.js'
 import { gitDiff } from '../utils/gitDiff.js'
 import { pushCommit } from '../utils/pushCommit.js'
 import { run } from '../utils/run.js'
 
+const isValidJson = scanner({
+  scripts: scanner({
+    build: string
+  })
+})
+
 export const build: Ghost = async () => {
+  const package_json = await getPackageJson()
+
+  if (!package_json) {
+    return {
+      status: 'skipped',
+      detail: 'Not found package.json in repo'
+    }
+  }
+
+  if (!isValidJson(package_json)) {
+    return {
+      status: 'skipped',
+      detail: 'Build command not found in package.json'
+    }
+  }
+
   const result = await run('npm run build')
 
   if (result.exitCode !== 0) {
