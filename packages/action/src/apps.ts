69551f92a8f33a8975f5ed0c5026f51fca32686b
jill64 <intents.turrets0h@icloud.com>
2024-03-01 15:38:14 +0900

    Revert "refactor ghost-merge"
    This reverts commit f332089281bd79380ae3c7d17fbba8293d692f30.

--- packages/action/src/apps.ts
+++ packages/action/src/apps.ts
@@ -7,6 +7,7 @@ import { deploy } from './ghosts/deploy.js'
 import { docs } from './ghosts/docs/index.js'
 import { format } from './ghosts/format.js'
 import { lint } from './ghosts/lint.js'
+import { merge } from './ghosts/merge/index.js'
 import { release } from './ghosts/release/index.js'
 
 export const apps = {
@@ -17,5 +18,6 @@ export const apps = {
   release,
   docs,
   bump,
+  merge,
   assign
 } satisfies Record<GhostName, Ghost>
---
+++ packages/action/src/ghosts/merge/index.ts
@@ -0,0 +1,68 @@
+import { Ghost } from '@/action/types/Ghost.js'
+import { attempt } from '@jill64/attempt'
+import { enableAutoMerge } from './enableAutoMerge.js'
+import { isAllowUsers } from './isAllowedUsers.js'
+
+export const merge: Ghost = async ({ payload, octokit }) => {
+  const {
+    owner,
+    repo,
+    data: { pull_number }
+  } = payload
+
+  if (!pull_number) {
+    return 'skipped'
+  }
+
+  const [{ data: pull_request }, { data: repository }] = await Promise.all([
+    octokit.rest.pulls.get({
+      owner,
+      repo,
+      pull_number
+    }),
+    octokit.rest.repos.get({
+      owner,
+      repo
+    })
+  ])
+
+  const allow = await isAllowUsers({
+    owner,
+    octokit,
+    name: pull_request.user.login,
+    ownerType: repository.owner.type
+  })
+
+  if (!allow) {
+    return {
+      status: 'skipped',
+      detail: 'This user is not allowed to merge'
+    }
+  }
+
+  const branch_protection = await attempt(
+    () =>
+      octokit.rest.repos.getBranchProtection({
+        owner,
+        repo,
+        branch: pull_request.base.ref
+      }),
+    null
+  )
+
+  if (!branch_protection?.data.required_status_checks?.contexts.length) {
+    return {
+      status: 'skipped',
+      detail: 'This repository does not have required status checks'
+    }
+  }
+
+  await enableAutoMerge({
+    repo,
+    owner,
+    octokit,
+    pull_number: pull_request.number
+  })
+
+  return 'success'
+}
