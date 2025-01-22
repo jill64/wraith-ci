e428da73681a89ddd25d5b388ecf4c9e0e6377b7
jill64 <contact@jill64.dev>
2025-01-22 13:07:45 +0900

    def ci

--- packages/cdk/src/cdk-stack.ts
+++ packages/cdk/src/cdk-stack.ts
@@ -13,19 +13,13 @@ class CDKStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    new DockerImageFunction(this, 'TimeTraveler', {
-      code: DockerImageCode.fromImageAsset('dist/time-traveler', {
+    new DockerImageFunction(this, 'TaskRunner', {
+      code: DockerImageCode.fromImageAsset('dist/task-runner', {
         platform: Platform.LINUX_AMD64
       }),
       architecture: aws_lambda.Architecture.X86_64,
-      timeout: Duration.seconds(30),
-      memorySize: 512,
-      environment: {
-        TIME_TRAVELER_SECRET: env.TIME_TRAVELER_SECRET!,
-        CLOUDFLARE_API_TOKEN: env.CLOUDFLARE_API_KEY!,
-        CLOUDFLARE_ACCOUNT_ID: env.CLOUDFLARE_ACCOUNT_ID!,
-        WRANGLER_LOG: 'none'
-      }
+      timeout: Duration.minutes(15),
+      memorySize: 512
     }).addFunctionUrl({
       authType: aws_lambda.FunctionUrlAuthType.NONE
     })
@@ -34,7 +28,7 @@ class CDKStack extends Stack {
 
 const app = new App()
 
-new CDKStack(app, `SolarSystem${suffix}`, {
+new CDKStack(app, `WraithCI${suffix}`, {
   env: {
     account: env.CDK_DEFAULT_ACCOUNT,
     region: env.CDK_DEFAULT_REGION
