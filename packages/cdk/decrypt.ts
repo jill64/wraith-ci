9cc1761d551b7b6fa7d5f5963bcbe0a311ea48ab
jill64 <contact@jill64.dev>
2025-01-22 12:07:37 +0900

    def decrypt

---
+++ packages/cdk/decrypt.ts
@@ -0,0 +1,29 @@
+import { text } from '@sveltejs/kit'
+import { env } from 'node:process'
+
+export const decrypt = async (encrypted_text: string) => {
+  const privateKey = await crypto.subtle.importKey(
+    'jwk',
+    JSON.parse(env.API_BRIDGE_PRIVATE_KEY!),
+    {
+      name: 'RSA-OAEP',
+      hash: { name: 'SHA-256' }
+    },
+    true,
+    ['decrypt']
+  )
+
+  const encrypted_buffer = new TextEncoder().encode(encrypted_text)
+
+  const decrypted_buffer = await crypto.subtle.decrypt(
+    {
+      name: 'RSA-OAEP'
+    },
+    privateKey,
+    encrypted_buffer
+  )
+
+  const decrypted_text = new TextDecoder().decode(decrypted_buffer)
+
+  return text(decrypted_text)
+}
