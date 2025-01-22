96a71338773238f465422a2c7dd6e7bfd4c765b8
jill64 <contact@jill64.dev>
2025-01-23 05:54:26 +0900

    resolve path

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './packages')
+      '$shared': path.resolve(__dirname, './packages/shared')
     }
   }
 })
