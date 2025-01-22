621e8377e1c6e7074f6e7394aa20ce56f247d55d
jill64 <contact@jill64.dev>
2025-01-22 15:10:46 +0900

    log

--- packages/cdk/src/task-runner/server.ts
+++ packages/cdk/src/task-runner/server.ts
@@ -3,6 +3,7 @@ import type { LambdaHandler, LambdaResponsePayload } from '@jill64/types-lambda'
 export const handler: LambdaHandler = async (
   event
 ): Promise<LambdaResponsePayload> => {
+  console.log(event)
   return {
     statusCode: 200,
     body: JSON.stringify(event)
