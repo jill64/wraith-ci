2ad86d911e22a54877447e4f5d98d3bd4d2b83b1
jill64 <contact@jill64.dev>
2025-01-22 09:41:19 +0900

    refactor: remove unused dependencies and commented code for clarity

--- packages/app/src/lib/server/auth/index.ts
+++ packages/app/src/lib/server/auth/index.ts
@@ -1,7 +1,7 @@
 import {
   AUTH0_CLIENT_ID,
   AUTH0_CLIENT_SECRET,
-  AUTH0_CUSTOM_DOMAIN,
+  // AUTH0_CUSTOM_DOMAIN,
   AUTH0_DOMAIN,
   SESSION_SECRET
 } from '$env/static/private'
@@ -11,7 +11,7 @@ import { CfAuth0 } from 'cf-auth0'
 export const auth = new CfAuth0({
   auth0ClientId: AUTH0_CLIENT_ID,
   auth0Domain: AUTH0_DOMAIN,
-  auth0CustomDomain: AUTH0_CUSTOM_DOMAIN,
+  // auth0CustomDomain: AUTH0_CUSTOM_DOMAIN,
   auth0ClientSecret: AUTH0_CLIENT_SECRET,
   callbackPath: '/api/auth/callback',
   loginPath: '/api/auth/login',
--- packages/app/src/routes/+layout.server.ts
+++ packages/app/src/routes/+layout.server.ts
@@ -1,7 +1,6 @@
 import { getSubscriptionNameFromEmail } from '$lib/server/billing/getSubscriptionNameFromEmail.js'
 import { run } from '$shared/db/run.js'
 import dayjs from 'dayjs'
-import { nanoid } from 'nanoid'
 
 export const load = async ({ locals }) => {
   const { db, oauth_user } = locals
@@ -10,13 +9,10 @@ export const load = async ({ locals }) => {
     await run(
       db.insertInto('user').values({
         oauth_id: oauth_user.id,
-        slug: nanoid(16),
-        name: oauth_user.name,
         created_at: new Date().toISOString(),
         updated_at: new Date().toISOString(),
         created_by: 0,
         updated_by: 0,
-        profile_image_key: '',
         plan_cache: 'FREE',
         plan_cached_at: new Date().toISOString()
       })
@@ -55,21 +51,6 @@ export const load = async ({ locals }) => {
         .set({
           plan_cache,
           plan_cached_at: new Date().toISOString()
-        })
-        .where('id', '=', me.id)
-    )
-  }
-
-  if (
-    !me.picture_cached_at ||
-    dayjs().diff(dayjs(me.picture_cached_at), 'day') > 1
-  ) {
-    await run(
-      db
-        .updateTable('user')
-        .set({
-          picture: oauth_user.picture,
-          picture_cached_at: new Date().toISOString()
         })
         .where('id', '=', me.id)
     )
