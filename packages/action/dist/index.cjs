45b5ffd35a3a0329894e36ceb015a1b2c155b61b
jill64 <contact@jill64.dev>
2025-01-26 13:05:58 +0900

    regenerate: artifact

--- packages/action/dist/index.cjs
+++ packages/action/dist/index.cjs
@@ -26264,7 +26264,7 @@ var pushCommit = async (message, run) => {
   await run("git config user.name wraith-ci[bot]");
   await run("git config user.email wraith-ci[bot]@users.noreply.github.com");
   await run("git add .");
-  await run(`git commit -m ${message}`);
+  await run(`git commit -m "${message}"`);
   await run("git pull --rebase");
   await run("git push origin");
 };
