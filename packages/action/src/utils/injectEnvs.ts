45f60c74171572b4d02f8bc3a7700dd10db79775
jill64 <contact@jill64.dev>
2025-01-27 10:10:18 +0900

    feat(utils): update injectEnvs to append .env to .gitignore if missing

--- packages/action/src/utils/injectEnvs.ts
+++ packages/action/src/utils/injectEnvs.ts
@@ -1,6 +1,6 @@
 import { decrypt } from '$shared/decrypt.js'
 import * as core from '@actions/core'
-import { writeFile } from 'node:fs/promises'
+import { readFile, writeFile } from 'node:fs/promises'
 import path from 'node:path'
 import { env } from 'node:process'
 import { findFile } from './findFile.js'
@@ -22,15 +22,27 @@ export const injectEnvs = async (encrypted_envs?: string): Promise<Run> => {
 
   const path_list = await findFile('package.json')
 
+  const gitIgnorePaths = await findFile('.gitignore')
+
   await Promise.all(
-    path_list.map((path_to_json) =>
-      writeFile(
+    gitIgnorePaths.map(async (path_to_gitignore) => {
+      const text = await readFile(path_to_gitignore)
+      const lines = text.toString().split('\n')
+      if (!lines.includes('.env')) {
+        await writeFile(path_to_gitignore, [...lines, '.env'].join('\n'))
+      }
+    })
+  )
+
+  await Promise.all(
+    path_list.map(async (path_to_json) => {
+      await writeFile(
         path.join(path_to_json.replace('package.json', ''), '.env'),
         Object.entries(json)
           .map(([key, value]) => `${key}=${value}`)
           .join('\n')
       )
-    )
+    })
   )
 
   return preRun(json)
