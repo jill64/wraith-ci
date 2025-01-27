7968a22fda2b18d3a830abc4c7f30c22d26d595f
jill64 <contact@jill64.dev>
2025-01-27 15:21:33 +0900

    feat(api): implement error handling for database queries in GitHub webhook

--- packages/app/src/routes/+layout.server.ts
+++ packages/app/src/routes/+layout.server.ts
@@ -27,39 +27,44 @@ export const load = async ({ locals }) => {
     return me
   }
 
-  const me =
-    (await db
-      .selectFrom('user')
-      .selectAll()
-      .where('user.github_user_id', '=', github_user.id)
-      .executeTakeFirst()) ?? (await createUser())
+  try {
+    const me =
+      (await db
+        .selectFrom('user')
+        .selectAll()
+        .where('user.github_user_id', '=', github_user.id)
+        .executeTakeFirst()) ?? (await createUser())
 
-  if (
-    dayjs().diff(dayjs(me.plan_cached_at), 'minute') > 1 ||
-    dayjs().diff(dayjs(me.created_at), 'minute') < 1
-  ) {
-    if (github_user.email) {
-      const plan_name = await getSubscriptionNameFromEmail(github_user.email)
+    if (
+      dayjs().diff(dayjs(me.plan_cached_at), 'minute') > 1 ||
+      dayjs().diff(dayjs(me.created_at), 'minute') < 1
+    ) {
+      if (github_user.email) {
+        const plan_name = await getSubscriptionNameFromEmail(github_user.email)
 
-      const plan_cache = plan_name.toUpperCase().split(' ')[0] as
-        | 'FREE'
-        | 'STARTER'
-        | 'PRO'
+        const plan_cache = plan_name.toUpperCase().split(' ')[0] as
+          | 'FREE'
+          | 'STARTER'
+          | 'PRO'
 
-      await run(
-        db
-          .updateTable('user')
-          .set({
-            plan_cache,
-            plan_cached_at: new Date().toISOString()
-          })
-          .where('id', '=', me.id)
-      )
+        await run(
+          db
+            .updateTable('user')
+            .set({
+              plan_cache,
+              plan_cached_at: new Date().toISOString()
+            })
+            .where('id', '=', me.id)
+        )
+      }
     }
-  }
 
-  return {
-    me,
-    github_user
+    return {
+      me,
+      github_user
+    }
+  } catch (e) {
+    console.error(e)
+    throw e
   }
 }
