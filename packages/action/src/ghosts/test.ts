7162b7eada7e492b24186f6e4d06b612e8bc86fd
jill64 <contact@jill64.dev>
2025-01-28 18:23:13 +0900

    feat: implement 'test' ghost to run npm test command and validate package.json

---
+++ packages/action/src/ghosts/test.ts
@@ -0,0 +1,38 @@
+import { scanner, string } from 'typescanner'
+import { Ghost } from '../../types/Ghost.js'
+import { getPackageJson } from '../utils/getPackageJson.js'
+
+const isValidJson = scanner({
+  scripts: scanner({
+    test: string
+  })
+})
+
+export const test: Ghost = async ({ run }) => {
+  const package_json = await getPackageJson()
+
+  if (!package_json) {
+    return {
+      status: 'skipped',
+      detail: 'Not found package.json in repo'
+    }
+  }
+
+  if (!isValidJson(package_json)) {
+    return {
+      status: 'skipped',
+      detail: 'Test command not found in package.json'
+    }
+  }
+
+  const testResult = await run('npm run test')
+
+  if (testResult.exitCode === 0) {
+    return 'success'
+  }
+
+  return {
+    status: 'failure',
+    detail: testResult.stderr
+  }
+}
